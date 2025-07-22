6c3bb305f89ff8369c198c0a3e4f3e8ecdbbbb42
PratikPatil3235 <pratikpatil@medianv.com>
2025-07-22 17:35:36 +0530

    commits made

--- src/manipulation/add.js
+++ src/manipulation/add.js
@@ -1,6 +1,13 @@
 import dayjs from "dayjs";
+import duration from "dayjs/plugin/duration.js"
 const a = dayjs();
 const b = a.add(7, "day");
 
+dayjs.extend(duration)
+
 console.log(a.format("DD-MMM-YYYY"));
 console.log(b.format("DD-MMM-YYYY"));
+
+const result = dayjs().add(dayjs.duration({'days' : 1}));
+
+console.log(result.format("DD-MMM-YYYY"))
