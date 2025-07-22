cee542d6a6ace01ba148ca4e2d8b634c7514570f
PratikPatil3235 <pratikpatil@medianv.com>
2025-07-22 15:30:19 +0530

    commits are made

---
+++ src/get-set/hour.js
@@ -0,0 +1,5 @@
+import dayjs from "dayjs";
+
+console.log(dayjs().hour());
+
+console.log(dayjs().hour(1).format());
---
+++ src/get-set/minute.js
@@ -0,0 +1,5 @@
+import dayjs from "dayjs";
+
+console.log(dayjs().minute());
+
+console.log(dayjs().minute(59));
