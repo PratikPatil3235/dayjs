b53a1cd9bf75405bcaaf0b91cbd34c92132474ed
PratikPatil3235 <pratikpatil@medianv.com>
2025-07-22 14:21:56 +0530

    commits made

---
+++ src/utc.js
@@ -0,0 +1,18 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+
+dayjs.extend(utc);
+
+console.log(dayjs().format());
+
+console.log(dayjs.utc().format());
+
+console.log(dayjs.utc().second(30).format("DD-MMM-YYYY"));
+
+const interval = setInterval(() => {
+  console.log(dayjs.utc().second());
+}, 1000);
+
+setTimeout(() => {
+  console.log("Interval cleared!");
+}, 5000);
