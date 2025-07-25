391d0466dabe5e297b6e6d8ca70ade8b296078c8
PratikPatil3235 <pratikpatil@medianv.com>
2025-07-25 09:31:08 +0530

    commits made

--- src/duration/duration.js
+++ src/duration/duration.js
@@ -4,4 +4,5 @@ import duration from "dayjs/plugin/duration.js";
 dayjs.extend(duration);
 
 const dur = dayjs.duration({ months: 12 });
+console.log(dayjs.duration(26, "hours")); // 1 day 2 hours ...
 console.log(dur.months()); // 12
